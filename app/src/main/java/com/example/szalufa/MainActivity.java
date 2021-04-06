e51eacfcd27ac3b1cbc4cfc5c5c310a29f9e1f76
Kacsa22 <kovacs.patrik@hotmail.com>
2021-04-06 21:09:05 +0200

    Szalufaköz számolás

--- app/src/main/java/com/example/szalufa/MainActivity.java
+++ app/src/main/java/com/example/szalufa/MainActivity.java
@@ -46,6 +46,5 @@ public class MainActivity extends AppCompatActivity {
         szam = Double.valueOf(Math.round(szam));
         szam = szam / Math.pow(10,jegy);
         return szam;
-        
     }
 }
